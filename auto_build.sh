8b147a0f915579a926305dd2583ecd814fb1458a
W.Byte <jack@WBytedeMacBook-Pro.local>
2020-12-16 15:28:32 +0800

    build(auto_build.sh): 添加自动部署脚本

---
+++ auto_build.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+CONTAINER_NAME="echo-front"
+
+container_id=$(docker ps -a | grep $CONTAINER_NAME | awk '{print $1}')
+
+if [ $container_id == "" ]
+then
+        echo "create"
+        docker run \
+            -d \
+            -p 80:80 \
+            -v $(pwd)/nginx.conf:/etc/nginx/conf.d/default.conf \
+            -v $(pwd)/build:/usr/share/nginx/html \
+            --name $CONTAINER_NAME
+            nginx
+
+else
+        echo "restart"
+        docker restart $container_id
+fi
